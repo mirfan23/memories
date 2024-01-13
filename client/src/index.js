8772270f9b002536ae3739e7606dcf65858e1364
mirfan23 <irfanfadhila000@gmail.com>
2024-01-13 15:39:53 +0700

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+import reducer from './reducers'
+
+import App from './App';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+const root = document.getElementById('root');
+
+const createRoot = ReactDOM.createRoot(root);
+createRoot.render(<App />);
